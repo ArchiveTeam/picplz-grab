e08b221711702c882535682008b08eba3759cad4
Alard <alard@example.com>
2012-06-03 12:51:25 +0200

    Better counting of followers/following.

--- picplz-user.lua
+++ picplz-user.lua
@@ -87,7 +87,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
         -- followers or following
 
         -- show to user
-        local n = value["user_count"]
+        local n = #(value["users"])
         if n == 1 then
           print(" - Discovered 1 user in "..api_section)
         else
